3e6aa41c7d22b64485947d358eb130a8c1898181
blaxians <113329221+blaxians@users.noreply.github.com>
2023-01-13 00:58:39 +0800

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,1 +1,7 @@
-Write-Host "Hello World"
+#This script will change the name of the computer, without requiring a restart
+
+#Prompt for user input
+$name = Read-Host -Prompt 'Please enter the new computer name'
+
+#Set the new name
+Rename-Computer -NewName $name
